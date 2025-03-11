f331e79423b6a1522c4b35961a10a882bad4ec8b
Eknous-P <ehovakimyan053@gmail.com>
2025-03-11 22:15:23 +0400

    setup build stuff

---
+++ BUILD.sh
@@ -0,0 +1,23 @@
+CXX=x86_64-w64-mingw32-g++
+LIB=`pwd`
+
+mkdir build
+mkdir build/tmp
+
+cp iostream.h build/tmp/iostream.h
+echo "header path: "${LIB}
+
+for i in */*.cpp; do
+  FNM=`basename ${i} .cpp`
+  echo "building "${i}"..."
+
+  cp ${i} build/tmp/${FNM}_tmp.cpp
+  # change void main to int main
+  sed -i -e 's/void\ main/int\ main/g'\
+      build/tmp/"${FNM}"_tmp.cpp
+  # add main return
+  sed -i -e 's/^}/return\ 0;}/g' \
+      build/tmp/"${FNM}"_tmp.cpp
+  
+  ${CXX} "build/tmp/${FNM}_tmp.cpp" -isystem ${LIB} -o "build/${FNM}.exe" -static -static-libgcc -static-libstdc++
+done
---
+++ iostream.h
@@ -0,0 +1,8 @@
+/* why?
+ * why is this a thing?
+ * why do we have to use this instead?
+ * why do we have to write void main() {} ???
+ */
+
+#include <iostream>
+using namespace std; // because apparently iostream.h doesnt require this line.........
