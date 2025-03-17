d93e3c922e2e5e267973fe245eebf07680587452
Eknous-P <ehovakimyan053@gmail.com>
2025-03-17 13:47:59 +0400

    263-270

---
+++ code/4/264.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>
+void main() {
+  double x[10];
+  int n,i;
+  cout<<"n=";cin>>n;
+  for (i=0; i<n; i++) {
+    cout<<"x["<<i<<"]=";cin>>x[i];
+  }
+  double max=0,min=0;
+  for (i=0; i<n; i++) {
+    if (x[i]>max) max=x[i];
+    if (x[i]<min) min=x[i];
+  }
+
+  cout<<max*min<<endl;
+}
---
+++ code/4/267.cpp
@@ -0,0 +1,18 @@
+#include <iostream.h>
+void main() {
+  double x[10];
+  int n,i,j;
+  cout<<"n=";cin>>n;
+  for (i=0; i<n; i++) {
+    cout<<"x["<<i<<"]=";cin>>x[i];
+  }
+  double max=0;
+  for (i=0; i<n; i++) {
+    if (x[i]>max) {
+      max=x[i];
+      j=i;
+    }
+  }
+
+  cout<<j<<endl;
+}
