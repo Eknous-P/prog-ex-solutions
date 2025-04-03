4ee45846627e8d5ea17abb205c83f58bb44e7883
Eknous-P <ehovakimyan053@gmail.com>
2025-04-03 15:13:40 +0400

    bro why

--- code/theotherbook/3.7/5.cpp
+++ code/theotherbook/3.7/5.cpp
@@ -2,16 +2,14 @@
 #include <cmath>
 
 void main() {
-    double S=3, sum1=0, sum2=0;
+    double S=3, sum=0;
     for(int i=1; i<=6; i++) {
         for(int j=1; j<=5; j++) {
-            sum2 += sqrt(sin(i) + cos(j));
+            sum += sqrt(sin(i) + cos(j));
         }
-        sum1 += pow(sin(i+1),2) + sum2;
-        sum2 = 0;
+        S += pow(sin(i+1),2) + sum;
+        sum = 0;
     }
-    
-    S+=sum1;
 
     cout<<S<<endl;
 }
