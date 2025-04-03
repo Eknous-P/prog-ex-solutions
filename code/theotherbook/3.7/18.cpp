308a7570be18896acbf05f454501b68274557b57
Eknous-P <ehovakimyan053@gmail.com>
2025-04-03 15:54:41 +0400

    fix

--- code/theotherbook/3.7/18.cpp
+++ code/theotherbook/3.7/18.cpp
@@ -4,7 +4,7 @@
 void main() {
     double W=1, sum=0;
     
-    for(double m=0; m<=5; m++) {
+    for(double m=0; m<=6; m++) {
         for(double n=1; n<=5; n++) {
             sum+=(n+m)/2;
         }
