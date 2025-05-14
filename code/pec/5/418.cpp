857250c230c33e060dec758f6c2454e5c086f7a9
Eknous-P <ehovakimyan053@gmail.com>
2025-05-14 16:29:14 +0400

    aaaaa

---
+++ code/pec/5/418.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main() {
+  int m=0;
+  int x[10][10];
+  int i,j;
+
+  cout<<"m=";
+  do {cin>>m;} while (m<2||m>10);
+  for (i=0; i<m; i++) {
+    for (j=0; j<m; j++) {
+      cout<<"x["<<i<<"]["<<j<<"]=";cin>>x[i][j];
+    }
+  }
+
+  int n=0;
+
+  for (i=0; i<m; i++) {
+    for (j=0; j<=m-i-1; j++) {
+      if (x[i][j]==0) n++;
+    }
+  }
+
+  cout<<n<<endl;
+  return 0;
+}
---
+++ code/pec/5/420.cpp
@@ -0,0 +1,27 @@
+#include <iostream.h>
+void main() {
+  int m=4;
+  int x[4][4];
+  int i,j;
+
+  cout<<"m=";cin>>m;
+  for (i=0; i<m; i++) {
+    for (j=0; j<m; j++) {
+      cout<<"x["<<i<<"]["<<j<<"]=";cin>>x[i][j];
+    }
+  }
+
+  double c=0;
+  int n=0;
+
+  for (i=0; i<=m; i++) {
+    for (j=0; j<=i; j++) {
+      if (x[i][j]%2==0) {
+        c+=x[i][j];
+        n++;
+      }
+    }
+  }
+
+  cout<<c/n<<endl;
+}
